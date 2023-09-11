a787ef256f12ec2cde018e105426f5a70a41262a
Gaurab Khanal <40165867+gaurab-khanal@users.noreply.github.com>
2023-09-11 22:43:02 +0545

    Update List.js

--- client/src/user/List.js
+++ client/src/user/List.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Base from "../core/Base";
 import Card from './../core/Card';
+import { useLoaderData } from 'react-router-dom';
 
 
 
 
 
 
-const List = ({ programs }) => {
-    console.log(programs);
+const List = () => {
+     const programsPromise = useLoaderData();
+
+     programsPromise
+    .then((programs) => {
+      if (!Array.isArray(programs)) {
+        throw new Error("Data is not in an array format");
+      }
+        
     return (
         <Base title="Programs">
 
